81e969e8a84dd736ee5208311c52ff3d91db33c4
Kty <katiuska.rodriguez5@gmail.com>
2021-05-29 10:57:51 +0000

    origin the database model for Instagramkty

--- src/models.py
+++ src/models.py
@@ -5,34 +5,53 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
-
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    last_name = Column(String(250))
+    email=Column(String(250), nullable=False)
+    password=Column(String(250), nullable=False)
+    # is_logged= Column(Boolean, default=False, nullable=False)
+class Characters(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    eye_color=Column(String(250))
+    skin_color=Column(String(250))
+    gender=Column(String(250))
+    height = Column(String(250))
+    description= Column(String(250))
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    population=Column(Integer,primary_key=False)
+    orbital_period=Column(Integer,primary_key=False)
+    rotation_period= Column(Integer,primary_key=False)
+    diameter = Column(Integer,primary_key=False)
+class Favorite_Character(Base):
+    __tablename__ = 'favorite_character'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Characters)
+class Favorite_Planet(Base):
+    __tablename__ = 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planets)
+def to_dict(self):
+        return {}
+render_er(Base, 'diagram.png')
+
+
+
+
+
+
